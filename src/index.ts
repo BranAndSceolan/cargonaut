bebc80415f463ea1de07becab336ea0f42974061
Antonia Strack <antonia.strack@mni.thm.de>
2022-06-02 09:28:38 +0200

    add link for opening in browser

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ const app: Application = express();
 const port = 5000;
 
 // Application routing
-app.use('/', (req: Request, res: Response, next: NextFunction ) => {
-    res.status(200).send({data: 'Hello from Ornio AS'});
+app.use('/', (req: Request, res: Response) => {
+    res.status(200).send({data: 'Hello from Cargonaut'});
 });
 
 // Start server
-app.listen(port, () => console.log(`Server is listening on port ${port}!`));
+app.listen(port, () => console.log(`Server is listening on http://localhost:${port}`));
