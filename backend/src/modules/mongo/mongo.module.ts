053dce7110567b17d16e75b49cd171ecbb1d1c74
Antonia Strack <antonia.strack@mni.thm.de>
2022-06-16 12:20:09 +0200

    add index for models

--- backend/src/modules/mongo/mongo.module.ts
+++ backend/src/modules/mongo/mongo.module.ts
@@ -1,6 +1,8 @@
-import {connect} from 'mongoose';
+import mongoose, {connect} from 'mongoose';
 import {dbName} from '../../config/config.json';
 import config from 'config'
+import * as schemes from "../../models/index"
+import {Vehicle} from "../../models/vehicle.model";
 
 /**
  * Basic functions for interacting with MongoDB
@@ -12,5 +14,11 @@ export class MongoModule {
      */
     async connectToMongo() {
         return connect(config.get('Database.mongoURL'), {dbName: dbName, serverSelectionTimeoutMS: 5000})
+    }
+
+    async addVehicle(vehicleData: Vehicle): Promise < mongoose.Types.ObjectId | null >{
+        const vehicle = new schemes.vehicleModel(vehicleData);
+        const i = await vehicle.save();
+        return i._id
     }
 }
