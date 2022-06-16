7986657fb7a077dbdb160356c215e9062b8012d8
Madiena <madelainehestermann@yahoo.de>
2022-06-16 17:34:55 +0200

    Add methods to evaluation.module

---
+++ backend/src/modules/entities/evaluation.module.ts
@@ -0,0 +1,74 @@
+import {EntityModule} from "./entity.module";
+import {MongoModule} from "../mongo/mongo.module";
+import {Evaluation, EvaluationClass} from "../../models/evaluation.model";
+import mongoose from "mongoose";
+import {printToConsole} from "../util/util.module";
+
+/**
+ * Module for evaluations, providing all label functionalities
+ *     for evaluation controller using methods of mongo module.
+ */
+export class LabelModule extends EntityModule {
+    constructor(mongo: MongoModule) {
+        super(mongo);
+    }
+
+    /**
+     * calls addeEvaluation() method of mongo.module, to create a new evaluation
+     * @param {Evaluation}
+     * @return {mongoose.Types.ObjectId|null} id for created evaluation
+     */
+    async createEvaluation(evaluationData: Evaluation): Promise<mongoose.Types.ObjectId | null> {
+        let evaluationId;
+        if (evaluationData && evaluationData.result && evaluationData.user && evaluationData.ride){
+            evaluationId = await this.mongo.addEvaluation(new EvaluationClass(evaluationData.result, evaluationData.ride, evaluationData.user));
+        }
+        if (evaluationId) {
+            printToConsole('[+] New evaluation with id ' + evaluationId.toString() + ' saved.');
+            return evaluationId
+        } else {
+            return null
+        }
+    }
+
+    /**
+     * calls findEvaluation() method of mongo.module, to find an evaluation with a specific id
+     * @param {mongoose.Types.ObjectId} id
+     * @return {Evaluation|null} evaluation
+     */
+    async findEvaluationById(id: mongoose.Types.ObjectId): Promise<Evaluation | null> {
+        return this.mongo.findEvaluation({_id: id});
+    }
+
+    /**
+     *  calls findEvaluations() method of mongo.module, to get all evaluations
+     *  @return {Evaluation[]} evaluations
+     */
+    async getAllEvaluations(): Promise<Evaluation[]> {
+        return this.mongo.findEvaluations({});
+    }
+
+    /**
+     * calls deleteEvaluation() method of mongo.module, to delete a vehicle specified by id
+     * @param {mongoose.Types.ObjectId} id
+     * @return {Vehicle|null} deleted vehicle
+     */
+    async deleteVehicle(id: mongoose.Types.ObjectId): Promise<Vehicle | null> {
+        return this.mongo.deleteVehicle(id);
+    }
+
+
+    /**
+     * calls deleteAllVehicles() method of mongo.module, to delete all vehicles
+     */
+    async deleteAllLabels() {
+        await this.mongo.deleteAllVehicles()
+            .then(() => {
+                printToConsole("[-] Deleted all vehicles")
+            })
+            .catch(err => {
+                printToConsole(err)
+            })
+    }
+
+}
