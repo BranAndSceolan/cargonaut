085e4a1b0f4b06781c12a187226f58e7f63e9c34
Madiena <madelainehestermann@yahoo.de>
2022-06-17 18:02:59 +0200

    Add request router

---
+++ backend/src/routes/request.router.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import {Request, Response} from 'express'
+import {requestController} from '../controllers';
+
+export const router = express.Router({
+    strict: true
+})
+
+/**
+ * User Routes
+ */
+
+// POST Routes
+router.post('/create', (req: Request, res: Response) => {
+    requestController.create(req, res)
+})
+
+// GET Routes
+router.get('/getAll', (req: Request, res: Response) => {
+    requestController.getAll(req,res)
+})
+
+router.get('/getByName/:name', (req: Request, res: Response) => {
+    requestController.get(req,res)
+})
+
+// DELETE Routes
+router.delete('/delete/:id', (req: Request, res: Response) => {
+    requestController.delete(req, res)
+})
+
