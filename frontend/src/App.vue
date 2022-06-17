5174993ab08a9b676e211d4bead7fc26c7a0a553
Andreas <andreas-wagner2@mni.thm.de>
2022-06-18 00:00:29 +0200

    card template done and few stubs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,17 @@
   <div id="app">
     <NavBar></NavBar>
     <nav>
+      <div>
+        <b-card-group deck>
+          <travel-card name="Test" start="Gießen" stop="Frankfurt" seats="3" room="2" price="19,99"></travel-card>
+          <travel-card name="Ich entführe euch alle" start="???" stop="Polen" seats="2" room="8" price="0,00"></travel-card>
+          <travel-card name="Free Candy" start="Pausenhof" stop="???" seats="2" room="8" price="0,00"></travel-card>
+          <travel-card name="Pazifik fahrt" start="Irrenhaus" stop="Marianengraben" seats="2" room="8" price="0,00"></travel-card>
+          <travel-card name="Free Ethanol" start="Keller" stop="Krankenhaus" seats="1" room="2" price="15000,00"></travel-card>
+          <travel-card name="*nuscheln*" start="???" stop="Bett" seats="?" room="?" price="0,00"></travel-card>
+          <travel-card name="Wirres Zeug" start="Dusche" stop="Nobelpreis" seats="?" room="?" price="0,00"></travel-card>
+        </b-card-group>
+      </div>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
@@ -35,7 +46,8 @@ nav a.router-link-exact-active {
 <script>
 import NavBar from '@/components/NavBar'
 import FootBar from '@/components/FootBar'
+import TravelCard from '@/components/travel-card'
 export default {
-  components: { FootBar, NavBar }
+  components: { TravelCard, FootBar, NavBar }
 }
 </script>
--- frontend/src/components/travel-card.vue
+++ frontend/src/components/travel-card.vue
@@ -1,13 +1,47 @@
 <template>
-  <div></div>
+  <div>
+    <b-card
+    :title=name
+    style="max-width: 20rem; min-width: 15rem"
+    class="mb-2 text-left"
+    >
+      <b-card-text>
+        Sitze: {{ seats }}
+      </b-card-text>
+      <b-card-text>
+        Platz: {{room}}m&sup3;
+      </b-card-text>
+      <b-card-text class="targets">
+        Start: {{start}}
+      </b-card-text>
+      <b-card-text class="targets float">
+        Ziel: {{stop}}
+        <b-card-text class="text-right" style="color: grey">{{price}}€</b-card-text>
+      </b-card-text>
+    </b-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'travel-card'
+  name: 'travel-card',
+  props: {
+    date: Date,
+    name: String,
+    seats: Number,
+    room: Number,
+    start: String,
+    layover: String,
+    stop: String,
+    price: Number
+  }
 }
+
 </script>
 
 <style scoped>
 
+.targets{
+  color: #005b52;
+}
 </style>
