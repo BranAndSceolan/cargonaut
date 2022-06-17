27026d57c77d82d7330aad2313256430393ab3dc
Andreas <andreas-wagner2@mni.thm.de>
2022-06-17 22:44:45 +0200

    navbar is now responsive and with bootstrap :)

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,14 +1,15 @@
 <template>
-  <nav class="navbar">
-
-      <div class="nav-elements">
-        <img src="../assets/cargonaut-white.png" class="logo" alt="Cargonaut logo">
-        <a href="/"> Angebot </a>
-        <a href="/"> Gesuche </a>
-      </div>
-
-    <div class="profil-icon">icon</div>
-  </nav>
+  <b-navbar class="navbar">
+    <b-navbar-brand>
+      <img src="../assets/cargonaut-white.png" class="logo" alt="Cargonaut Logo">
+    </b-navbar-brand>
+    <b-navbar-nav>
+      <b-nav-item href="#" class="link"> Angebote </b-nav-item>
+    </b-navbar-nav>
+    <b-navbar-nav class="ml-auto">
+      <div class="link">ICON</div>
+    </b-navbar-nav>
+  </b-navbar>
 
 </template>
 
@@ -21,43 +22,13 @@ export default {
 <style scoped>
 .navbar{
   background: #005b52;
-  height: 100px;
-  position: relative;
-  top: -8px;
-  width: 97%;
-  padding-right: 43px;
-  margin-left: -8px;
-
 }
 .logo{
-  color: white;
-  height: 70px;
-  margin-top: 10px;
-  margin-left: 50px;
-  margin-right: 20px;
-  float: left;
+  height: 60px;
 }
-.nav-elements{
-  width: 60%;
-}
-
-.nav-elements a{
-  text-decoration: none;
+.link{
   color: white;
-  font-size: 30px;
-  margin: 25px;
-  float: left;
-  margin-top: 30px;
-
-}
-
-.profil-icon{
-  margin-right: 150px;
-  margin-top: 30px;
-  font-size: 30px;
-  color: white;
-  float: right;
-
+  font-size: 25px;
 }
 
 </style>
