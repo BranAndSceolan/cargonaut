4a4d1635cf0cb0991b49679029be155c94013fc2
Andreas <andreas-wagner2@mni.thm.de>
2022-06-27 18:42:41 +0200

    create register page

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -6,23 +6,23 @@
     <b-card-body class="body">
       <div class="info">
         <div class="head"> Registrieren </div>
-        <b-input-group id="title" class="title">
+        <b-input-group id="title" class="name">
           <b-form-input v-model="name" placeholder="Name" class="input shadow-sm"></b-form-input>
         </b-input-group>
-        <b-input-group id="sitze" class="seat">
+        <b-input-group id="sitze" class="date">
           <b-form-input v-model="date" placeholder="Geburtsdatum" class="input shadow-sm"></b-form-input>
         </b-input-group>
-        <b-input-group id="platz" class="space">
+        <b-input-group id="platz" class="email">
           <b-form-input v-model="email" placeholder="E-Mail" class="input shadow-sm"></b-form-input>
         </b-input-group>
         <b-input-group id="desc" class="desc">
           <b-form-input v-model="desc" placeholder="Beschreibung" class="input shadow-sm"></b-form-input>
         </b-input-group>
-        <b-input-group id="desc" class="desc">
-          <b-form-input v-model="password1" placeholder="Passwort" class="input shadow-sm"></b-form-input>
+        <b-input-group id="desc" class="pw1">
+          <b-form-input v-model="password1" placeholder="Passwort" type="password" class="input shadow-sm"></b-form-input>
         </b-input-group>
-        <b-input-group id="desc" class="desc">
-          <b-form-input v-model="password2" placeholder="Passwort (Wdh.)" class="input shadow-sm"></b-form-input>
+        <b-input-group id="desc" class="pw2">
+          <b-form-input v-model="password2" placeholder="Passwort (Wdh.)" type="password" class="input shadow-sm"></b-form-input>
         </b-input-group>
       </div>
     </b-card-body>
