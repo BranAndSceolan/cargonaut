b934c091b00b50e4d42ee844063a27733d264e04
Andreas <andreas-wagner2@mni.thm.de>
2022-06-28 17:57:46 +0200

    login works again

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -38,8 +38,8 @@ export default {
       if (this.user !== '' && this.password !== '') {
         axios.post('/user/login',
           {
-            signInName: this.user,
-            signInPass: this.password
+            name: this.user,
+            password: this.password
           })
           .then().catch(reason => {
             console.log(reason)
